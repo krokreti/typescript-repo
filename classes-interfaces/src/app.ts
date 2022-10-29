08e38b8d98f8a8114c5ac107f5bbdb9f4a584e58
Davi Pinheiro <davipinheiro1993@gmail.com>
2022-10-29 12:26:53 -0300

    Update app.ts

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,13 +1,17 @@
 class Department {
     //o nome de cada campo é field (em ingles)
-    name: string;
+    // name: string;
     //colocando valor default em um campo
     teste: string = 'Field';
     
+    private employees:string[] = [];
+
     //construtor é executado quando um objeto/classe é criado
-    constructor(n: string) {
+    //no construtor podemos colocar propriedades que queremos que sejam criadas e inicializadas (quando fizer isso, temos q especificar se são public ou private)
+    //essa forma de incializar é só um atalho, nao precisa ser necessariamente assim
+    constructor(private id: number, public age: number, public name: string, teste: string='default') {
         //ele vai passar o valor do parametro para o name quando criado
-        this.name = n;
+        this.teste = teste;
     }
 
     describe() {
@@ -16,10 +20,28 @@ class Department {
         // o this referencia pra esta instancia criada
         console.log('Department: ' + this.name)
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 //instanciando um objeto da classe Department
 
-const accounting = new Department('Accounting');
+const accounting = new Department(1, 15,'Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu')
+
+//isto é uma má pratica, pq faz com q tenha varias formas de se adicionar, e o ideal é uma
+//por isso, o campo employee deve ser privado, para que as variaveis nao tenham acesso direto para modifica-la
+// accounting.employees[2] = 'Clovis'
+
+accounting.printEmployeeInformation();
 
 console.log(accounting);
